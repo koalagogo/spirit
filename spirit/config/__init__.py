8b02851416bc2d5decaf8e08a4cba12b249afcbd
zhuyan <zhuyan@intern.douban.com>
2016-04-29 22:11:51 +0800

    simple skeleton

---
+++ spirit/config/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .base import load_config
+
+
+all = ['load_config']
---
+++ spirit/config/production.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .base import Config
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = ProductionConfig()
